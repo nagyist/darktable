nagyist/darktable

commit b34c81b83cf6ac31dd4329e8b9817cbd64edcbc2

Avoid some superfluous full pixelpipe runs
When zooming in/out in darkroom we had some extra pixelpipe runs due to bad restarting
in dt_dev_process_image_job().
As we take care for redrawing via a signal later, the removed part of the code was just bad.
The penalty was mostly not that bad because of caching but that depends on memory and on OpenCL
if a module late in the pipe had cl_mem saved. But clearly bad.

src/develop/develop.c
@@ -811,14 +811,6 @@ restart:
                   dev->image_storage.filename);
   _dev_average_delay_update(&start, &pipe->average_delay);
 
-  // maybe we got zoomed/panned in the meantime?
-  if(port && pipe->changed != DT_DEV_PIPE_UNCHANGED)
-  {
-    if(port->widget && !stopped)
-      dt_control_queue_redraw_widget(port->widget);
-    goto restart;
-  }
-
   pipe->status = DT_DEV_PIXELPIPE_VALID;
   pipe->loading = FALSE;
   dev->image_invalid_cnt = 0;

commit f1d62e492025b3ed198316821933fc9b830a781d

Image pixelpipe restart fix
The `Avoid some superfluous full pixelpipe runs` commit b34c81b83cf6ac31dd4329e8b9817cbd64edcbc2
broke pipe processing for image pipes.
In that case we need the final image dimensions so we restart.

While being here:
1. We should give some slightly better hints for pixelpipe stop reports and document in code.
2. introduced tests dt_pipe_is_image() and dt_pipe_is_image_final()
   and consequently used all those tests all over the code.
3. A few minor code simplifications and constify

src/develop/develop.c
@@ -779,7 +779,7 @@ restart:
     dt_print_pipe(DT_DEBUG_PIPE, "pipe stopped", pipe, NULL, DT_DEVICE_NONE, &proi, NULL, "%s%s%s%s",
                 shutdown == DT_DEV_PIXELPIPE_STOP_NODES ? "DT_DEV_PIXELPIPE_STOP_NODES"
                   : shutdown == DT_DEV_PIXELPIPE_STOP_HQ  ? "DT_DEV_PIXELPIPE_STOP_HQ"
-                  : shutdown == DT_DEV_PIXELPIPE_STOP_NO  ? "" : "DT_DEV_PIXELPIPE_STOP_OTHER",
+                  : shutdown == DT_DEV_PIXELPIPE_STOP_NO  ? "" : "DT_DEV_PIXELPIPE_STOP_MODULE",
                 dev->image_force_reload ? "image_force_reload " : "",
                 pipe->loading ? "pipe_loading " : "",
                 pipe->input_changed ? "pipe_input_changed " : "");
@@ -802,9 +802,17 @@ restart:
       dt_pthread_mutex_unlock(&pipe->mutex);
       return;
     }
+
+    /* pixelpipe stops due to changed pipe nodes, HQ mode changes or module aborts.
+       All want restarts as pipe status is not valid yet.
+    */
     if(shutdown != DT_DEV_PIXELPIPE_STOP_NO)
       goto restart;
   }
+
+  // image pipes require pending dimension check
+  if(port && dt_pipe_is_image(pipe) && pipe->changed != DT_DEV_PIPE_UNCHANGED)
+    goto restart;
 
   dt_show_times_f(&start,
                   "[dev_process_image] pixel pipeline", "processing `%s'",
@@ -3860,7 +3868,7 @@ void dt_dev_image(const dt_imgid_t imgid,
   dev.gui_attached = FALSE;
   dt_dev_pixelpipe_t *pipe = dev.full.pipe;
 
-  pipe->type |= DT_DEV_PIXELPIPE_IMAGE | (finalscale ? DT_DEV_PIXELPIPE_IMAGE_FINAL : 0);
+  pipe->type |= DT_DEV_PIXELPIPE_IMAGE | (finalscale ? DT_DEV_PIXELPIPE_IMAGE_FINAL : DT_DEV_PIXELPIPE_NONE);
   // load image and set history_end
 
   dev.snapshot_id = snapshot_id;

src/develop/pixelpipe_hb.c
@@ -50,7 +50,7 @@
 // benchmarking and pfm dumps should happen for these pipes
 static inline gboolean _is_debug_pipe(const dt_dev_pixelpipe_t *pipe)
 {
-  return (pipe->type & (DT_DEV_PIXELPIPE_FULL | DT_DEV_PIXELPIPE_EXPORT));
+  return dt_pipe_is_full(pipe) || dt_pipe_is_export(pipe);
 }
 
 // forward declarations for mask cache helpers
@@ -1107,16 +1107,7 @@ static gboolean _transform_for_blend(const dt_iop_module_t *const self,
                                      const dt_dev_pixelpipe_iop_t *const piece)
 {
   const dt_develop_blend_params_t *const d = piece->blendop_data;
-  if(d)
-  {
-    // check only if blend is active
-    if((self->flags() & IOP_FLAGS_SUPPORTS_BLENDING)
-       && (d->mask_mode != DEVELOP_MASK_DISABLED))
-    {
-      return TRUE;
-    }
-  }
-  return FALSE;
+  return d && (d->mask_mode != DEVELOP_MASK_DISABLED) && (self->flags() & IOP_FLAGS_SUPPORTS_BLENDING);
 }
 
 static gboolean _request_color_pick(dt_dev_pixelpipe_t *pipe,
@@ -1267,9 +1258,7 @@ static inline gboolean _piece_wants_blending(const dt_dev_pixelpipe_iop_t *piece
     return FALSE;
 
   const dt_develop_blend_params_t *const d = piece->blendop_data;
-  if(!d || !(d->mask_mode & DEVELOP_MASK_ENABLED)) return FALSE;
-
-  return TRUE;
+  return d && (d->mask_mode & DEVELOP_MASK_ENABLED);
 }
 
 static gboolean _pixelpipe_process_on_CPU(dt_dev_pixelpipe_t *pipe,
@@ -3068,7 +3057,7 @@ gboolean dt_dev_pixelpipe_process(dt_dev_pixelpipe_t *pipe,
                                   const int devid)
 {
   pipe->processing = TRUE;
-  pipe->nocache = (pipe->type & DT_DEV_PIXELPIPE_IMAGE) != 0;
+  pipe->nocache = dt_pipe_is_image(pipe);
   pipe->runs++;
   pipe->opencl_enabled = dt_opencl_running();
 
@@ -3745,7 +3734,7 @@ gboolean dt_dev_write_scharr_mask(dt_dev_pixelpipe_iop_t *piece,
 
 #ifdef HAVE_OPENCL
 int dt_dev_write_scharr_mask_cl(dt_dev_pixelpipe_iop_t *piece,
-                                cl_mem in,
+                                const cl_mem in,
                                 const dt_iop_roi_t *const roi,
                                 const gboolean rawmode)
 {

src/develop/pixelpipe_hb.h
@@ -268,6 +268,14 @@ static inline gboolean dt_pipe_is_screen(const dt_dev_pixelpipe_t *pipe)
 {
   return (pipe->type & DT_DEV_PIXELPIPE_SCREEN);
 }
+static inline gboolean dt_pipe_is_image(const dt_dev_pixelpipe_t *pipe)
+{
+  return (pipe->type & DT_DEV_PIXELPIPE_IMAGE);
+}
+static inline gboolean dt_pipe_is_image_final(const dt_dev_pixelpipe_t *pipe)
+{
+  return (pipe->type & DT_DEV_PIXELPIPE_IMAGE_FINAL);
+}
 static inline gboolean dt_pipe_no_mask_display(const dt_dev_pixelpipe_t *pipe)
 {
   return pipe->mask_display == DT_DEV_PIXELPIPE_DISPLAY_NONE;
@@ -390,7 +398,7 @@ gboolean dt_dev_write_scharr_mask(dt_dev_pixelpipe_iop_t *piece,
                                   const gboolean mode);
 #ifdef HAVE_OPENCL
 int dt_dev_write_scharr_mask_cl(dt_dev_pixelpipe_iop_t *piece,
-                                cl_mem in,
+                                const cl_mem in,
                                 const dt_iop_roi_t *const roi_in,
                                 const gboolean mode);
 #endif

src/iop/ashift.c
@@ -4053,7 +4053,7 @@ static int call_distort_transform(const dt_develop_t *dev,
   if(piece->module == self
       // && *piece->enabled see note below
       && !(dt_iop_module_is_skipped(dev, piece->module)
-            && (pipe->type & DT_DEV_PIXELPIPE_BASIC)))
+            && dt_pipe_is_basic(pipe)))
   {
     ret = piece->module->distort_transform(piece->module, piece, points, points_count);
   }

src/iop/cacorrect.c
@@ -257,7 +257,7 @@ void process(dt_iop_module_t *self,
 
   const uint32_t filters = piece->filters;  // Just in case we want to use a roi_in 
 
-  const gboolean run_fast = piece->pipe->type & DT_DEV_PIXELPIPE_FAST;
+  const gboolean run_fast = dt_pipe_is_fast(piece->pipe);
 
   dt_iop_cacorrect_data_t *d = piece->data;
 

src/iop/channelmixerrgb.c
@@ -2159,7 +2159,7 @@ void process(dt_iop_module_t *self,
 #ifdef AI_ACTIVATED
     gboolean exit = FALSE;
 #endif
-    if(g->run_profile && piece->pipe->type == DT_DEV_PIXELPIPE_PREVIEW)
+    if(g->run_profile && dt_pipe_is_preview(piece->pipe))
     {
       dt_iop_gui_enter_critical_section(self);
       _extract_color_checker(in, out, roi_in, g, RGB_to_XYZ,
@@ -2284,7 +2284,7 @@ void process(dt_iop_module_t *self,
 
   // run dE validation at output
   if(self->dev->gui_attached && g)
-    if(g->run_validation && piece->pipe->type == DT_DEV_PIXELPIPE_PREVIEW)
+    if(g->run_validation && dt_pipe_is_preview(piece->pipe))
     {
       _validate_color_checker(out, roi_out, g, RGB_to_XYZ, XYZ_to_RGB, XYZ_to_CAM);
       g->run_validation = FALSE;
@@ -3130,7 +3130,7 @@ void commit_params(dt_iop_module_t *self,
   convert_any_XYZ_to_LMS(XYZ, d->illuminant, d->adaptation);
   d->illuminant[3] = 0.f;
 
-  const gboolean preview = piece->pipe->type == DT_DEV_PIXELPIPE_PREVIEW;
+  const gboolean preview = dt_pipe_is_preview(piece->pipe);
   const gboolean run_profile = preview && g && g->run_profile;
   const gboolean run_validation = preview && g && g->run_validation;
 

src/iop/colorequal.c
@@ -989,7 +989,7 @@ void process(dt_iop_module_t *self,
   const dt_iop_colorequal_gui_data_t *g = self->gui_data;
   const gboolean fullpipe = dt_pipe_is_full(piece->pipe);
   const int mask_mode = g && fullpipe ? g->mask_mode : 0;
-  const gboolean run_fast = piece->pipe->type & DT_DEV_PIXELPIPE_FAST;
+  const gboolean run_fast = dt_pipe_is_fast(piece->pipe);
 
   const float *const restrict in = (float*)i;
   float *const restrict out = (float*)o;
@@ -1531,7 +1531,7 @@ int process_cl(dt_iop_module_t *self,
   const gboolean fullpipe = dt_pipe_is_full(piece->pipe);
   const int mask_mode = g && fullpipe ? g->mask_mode : 0;
   const int guiding = d->use_filter;
-  const gboolean run_fast = piece->pipe->type & DT_DEV_PIXELPIPE_FAST;
+  const gboolean run_fast = dt_pipe_is_fast(piece->pipe);
 
   float white, sat_shift, max_brightness_shift, corr_max_brightness_shift, bright_shift, gradient_amp, hue_sigma, par_sigma, sat_sigma, scharr_sigma;
   _prepare_process(roi_in->scale / piece->iscale, d,

src/iop/colorin.c
@@ -1324,7 +1324,7 @@ void commit_params(dt_iop_module_t *self,
     piece->enabled = FALSE;
     return;
   }
-  if(!(piece->pipe->type & DT_DEV_PIXELPIPE_IMAGE_FINAL))
+  if(!dt_pipe_is_image_final(piece->pipe))
     piece->enabled = TRUE;
 
   if(type == DT_COLORSPACE_ENHANCED_MATRIX)

src/iop/crop.c
@@ -622,7 +622,7 @@ void commit_params(dt_iop_module_t *self,
   dt_iop_crop_params_t *p = (dt_iop_crop_params_t *)p1;
   dt_iop_crop_data_t *d = piece->data;
 
-  if(dt_iop_has_focus(self) && (pipe->type & DT_DEV_PIXELPIPE_BASIC))
+  if(dt_iop_has_focus(self) && dt_pipe_is_basic(pipe))
   {
     d->cx = 0.0f;
     d->cy = 0.0f;

src/iop/diffuse.c
@@ -1344,7 +1344,7 @@ void process(dt_iop_module_t *self,
              const dt_iop_roi_t *const roi_in,
              const dt_iop_roi_t *const roi_out)
 {
-  const gboolean fastmode = piece->pipe->type & DT_DEV_PIXELPIPE_FAST;
+  const gboolean fastmode = dt_pipe_is_fast(piece->pipe);
 
   const dt_iop_diffuse_data_t *const data = piece->data;
 
@@ -1607,7 +1607,7 @@ int process_cl(dt_iop_module_t *self,
                const dt_iop_roi_t *const roi_in,
                const dt_iop_roi_t *const roi_out)
 {
-  const gboolean fastmode = piece->pipe->type & DT_DEV_PIXELPIPE_FAST;
+  const gboolean fastmode = dt_pipe_is_fast(piece->pipe);
 
   const dt_iop_diffuse_data_t *const data = piece->data;
   dt_iop_diffuse_global_data_t *const gd = self->global_data;

src/iop/dither.c
@@ -671,7 +671,7 @@ void process(
     _process_posterize(self, piece, ivoid, ovoid, roi_in, roi_out);
   else
   {
-    const gboolean fastmode = piece->pipe->type & DT_DEV_PIXELPIPE_FAST;
+    const gboolean fastmode = dt_pipe_is_fast(piece->pipe);
     _process_floyd_steinberg(self, piece, ivoid, ovoid, roi_in, roi_out, fastmode);
   }
 }

src/iop/filmicrgb.c
@@ -2063,7 +2063,7 @@ void tiling_callback(dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece,
   const int scales = get_scales(roi_in, piece);
   const int max_filter_radius = (1 << scales);
   const dt_iop_filmicrgb_data_t *const data = piece->data;
-  const gboolean run_fast = !data->enable_highlight_reconstruction || piece->pipe->type & DT_DEV_PIXELPIPE_FAST;
+  const gboolean run_fast = !data->enable_highlight_reconstruction || dt_pipe_is_fast(piece->pipe);
 
   // without reconstruction: in + out + 1ch_mask
   // with reconstruction: in + out + reconst + inpaint + 2 * scales + temp + 1ch_mask
@@ -2128,7 +2128,7 @@ void process(dt_iop_module_t *self,
     }
   }
 
-  const gboolean run_fast = piece->pipe->type & DT_DEV_PIXELPIPE_FAST;
+  const gboolean run_fast = dt_pipe_is_fast(piece->pipe);
   // allocate reconstruction buffer, but only if we actually want to use it
   float *const restrict reconstructed
     = run_fast ? NULL : dt_alloc_align_float((size_t)roi_out->width * roi_out->height * 4);
@@ -2463,7 +2463,7 @@ int process_cl(dt_iop_module_t *self,
     }
   }
 
-  const gboolean run_fast = piece->pipe->type & DT_DEV_PIXELPIPE_FAST;
+  const gboolean run_fast = dt_pipe_is_fast(piece->pipe);
 
   if(!run_fast && is_clipped > 0 && d->enable_highlight_reconstruction)
   {

src/iop/finalscale.c
@@ -58,10 +58,8 @@ dt_iop_colorspace_type_t default_colorspace(dt_iop_module_t *self,
 
 static inline gboolean _gui_fullpipe(dt_dev_pixelpipe_iop_t *piece)
 {
-  return piece->pipe->type & (DT_DEV_PIXELPIPE_FULL
-                              | DT_DEV_PIXELPIPE_PREVIEW2
-                              | DT_DEV_PIXELPIPE_IMAGE)
-    && darktable.develop->late_scaling.enabled;
+  return (dt_pipe_is_canvas(piece->pipe) || dt_pipe_is_image(piece->pipe))
+     && darktable.develop->late_scaling.enabled;
 }
 
 void modify_roi_in(dt_iop_module_t *self,
@@ -194,9 +192,8 @@ void commit_params(dt_iop_module_t *self,
                    dt_dev_pixelpipe_t *pipe,
                    dt_dev_pixelpipe_iop_t *piece)
 {
-  const int use_finalscale = DT_DEV_PIXELPIPE_IMAGE | DT_DEV_PIXELPIPE_IMAGE_FINAL;
   piece->enabled = dt_pipe_is_export(piece->pipe)
-                  || (pipe->type & use_finalscale) == use_finalscale
+                  || (dt_pipe_is_image(pipe) && dt_pipe_is_image_final(pipe))
                   || _gui_fullpipe(piece);
 }
 

src/iop/grain.c
@@ -442,7 +442,7 @@ void process(dt_iop_module_t *self,
 
   unsigned int hash = _hash_string(piece->pipe->image.filename) % (int)fmax(roi_out->width * 0.3, 1.0);
 
-  const gboolean fastmode = piece->pipe->type & DT_DEV_PIXELPIPE_FAST;
+  const gboolean fastmode = dt_pipe_is_fast(piece->pipe);
   // Apply grain to image
   const float strength = (data->strength / 100.0f);
   // double zoom=1.0+(8*(data->scale/100.0));

src/iop/hazeremoval.c
@@ -637,9 +637,8 @@ void process(dt_iop_module_t *self,
     dt_iop_gui_leave_critical_section(self);
   }
 
-  // FIXME in pipe->type |= DT_DEV_PIXELPIPE_IMAGE mode we currently can't receive data from preview
-  // so we at least leave a note to the user
-  if((piece->pipe->type & DT_DEV_PIXELPIPE_IMAGE) && !hq)
+  // As we can't receive data from preview we at least leave a note to the user
+  if(dt_pipe_is_image(piece->pipe) && !hq)
     dt_control_log(_("inconsistent output"));
 
   // In all other cases we calculate distance_max and A0 here.
@@ -907,9 +906,8 @@ int process_cl(dt_iop_module_t *self,
     dt_iop_gui_leave_critical_section(self);
   }
 
-  // FIXME in pipe->type |= DT_DEV_PIXELPIPE_IMAGE mode we currently can't receive data from preview
-  // so we at least leave a note to the user
-  if((piece->pipe->type & DT_DEV_PIXELPIPE_IMAGE) && !hq)
+  // As we can't receive data from preview we at least leave a note to the user
+  if(dt_pipe_is_image(piece->pipe) && !hq)
     dt_control_log(_("inconsistent output"));
 
   // In all other cases we calculate distance_max and A0 here.

src/iop/toneequal.c
@@ -1053,9 +1053,9 @@ void toneeq_process(dt_iop_module_t *self,
     }
     else if(dt_pipe_is_preview(piece->pipe))
     {
-      // For DT_DEV_PIXELPIPE_PREVIEW, we need to cache it too to
-      // compute the full image stats upon user request in GUI threads
-      // locks are required since GUI reads and writes on that buffer.
+      // For preview pipe we need to cache it too because we have to
+      // compute the full image stats upon user request in GUI threads.
+      // Locks are required since GUI reads and writes on that buffer.
 
       // Re-allocate a new buffer if the thumb preview size has changed
       dt_iop_gui_enter_critical_section(self);